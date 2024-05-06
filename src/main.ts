06251295313832778b41ffc83a910562eb64175b
Tuyendn <tuyendao181@gmail.com>
2024-05-06 23:06:32 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import {BootstrapVue3} from 'bootstrap-vue-3'
+import 'boxicons'
+// Import Bootstrap and BootstrapVue CSS files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+//boxicons
+import 'boxicons/css/boxicons.min.css';
+//common css
+import './assets/scss/theme-default.scss'
+import './assets/scss/core.scss'
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
